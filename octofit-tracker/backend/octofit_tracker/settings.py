2a5f93fadb7e6fa7290020adcd51f619f8d8072f
aktapaz <88380135+aktapaz@users.noreply.github.com>
2026-01-12 15:52:33 +0000

    checking REST API

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-*$84vyc65st_7-&j(oy#d2*9wbk94s*5&ewsjl-#4-3(xv-%+s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f"{CODESPACE_NAME}-8000.app.github.dev")
 
 
 # Application definition
